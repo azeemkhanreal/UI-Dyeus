54c510c1576e92c5694accbff0effc7a81cdea0f
Azeem Khan <azeemkhanreal@gmail.com>
2022-02-25 16:08:26 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const menuHandler = (e) => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="header">
+      <div
+        className={`header__menu ${menuOpen ? "active" : ""}`}
+        onClick={menuHandler}
+      >
+        <span></span>
+      </div>
+      <div className="header__cart">
+        <img src="/cart.svg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Product.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Product = () => {
+  return <div className="product"></div>;
+};
+
+export default Product;
